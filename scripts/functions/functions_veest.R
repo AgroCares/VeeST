bf14eea07f3fc0d07b935930d2f025504ad520ce
laura <lauramoria@gmail.com>
2024-07-15 15:43:17 +0200

    update import penetrometer profielen

--- scripts/functions/functions_veest.R
+++ scripts/functions/functions_veest.R
@@ -13,8 +13,7 @@ importGPS <- function(inputdir = paste0(workspace,"./GPS")){
   gps <- st_zm(gps, crs = 28992)
   return(gps)
 }
-
-# load gps in csv format
+# load gps in csv format ----------------------------------------------------------
 importGPS2 <- function(inputdir = paste0(workspace,"./GPS"), gpsid = 36){
   # load all results from aquo kit from input dir
   gps2 <- list.files(path= paste0(inputdir), pattern=".csv", full.names =  T)
@@ -28,7 +27,31 @@ importGPS2 <- function(inputdir = paste0(workspace,"./GPS"), gpsid = 36){
   gps2 <- st_as_sf(gps2, coords = c('x','y','z'), crs = 28992)
   return(gps2)
 }
-
+# load gps profielen     ----------------------------------------------------------
+importGPSprof <- function(inputdir = paste0(workspace,"./GPS slootprofielen"), gpsid = 36){
+  # load all results from aquo kit from input dir
+  prof <- list.files(path= paste0(inputdir), pattern=".csv", full.names =  T)
+  names <- gsub(paste0(inputdir,'/'), "", prof)
+  names <- gsub(paste0('.csv'), "", names)
+  dt.names <- data.table(ID = 1:length(names), name = names)
+  prof <- lapply( prof, fread, stringsAsFactors = FALSE)
+  prof <- rbindlist( prof, fill =T, use.names = T, idcol = "ID")
+  prof <- merge(dt.names,  prof , by = "ID")
+  #extract label waterlijn
+  prof[grepl('waterlijn', Opmerking), wl:= z]
+  prof[, wl_min := min(wl, na.rm = TRUE), by = "ID"]
+  prof[, wl := mean(wl, na.rm = TRUE), by = "ID"]
+  prof[grepl('waterlijn', Opmerking), numwl := Puntnummer]
+  prof[, numwl_min := min(numwl, na.rm = TRUE), by = "ID"]
+  prof[, numwl_max := max(numwl, na.rm = TRUE), by = "ID"]
+  prof[Puntnummer > numwl_max | Puntnummer < numwl_min, wl := NA]
+  #extract slibdikte
+  prof[, slib := as.numeric(Opmerking)/100]
+  prof[is.na(slib), slib:= 0]
+  
+  prof <- st_as_sf(prof, coords = c('x','y','z'), crs = 28992, remove = FALSE)
+  return(prof)
+}
 # Import penetrometerdata in txt format -------------------------------------------
 importPen <- function(inputdir = paste0(workspace,"./Penetrometer")){
   
@@ -50,29 +73,64 @@ importPen <- function(inputdir = paste0(workspace,"./Penetrometer")){
   # trim character columns from starting and ending space
   cols <- colnames(penheader)[sapply(penheader, is.character)] # which colnames are character
   penheader <- penheader[,(cols) := lapply(.SD, function(x) gsub("^\\s+|\\s+$", "", x)),.SDcols = cols]
-  
-  # names header differ, force one unique name
-  dt.names <- data.table(ID = 1:length(names), name = names)
   penheader <- data.table(ID = penheader$ID, header = rep(penheader$header[penheader$ID == 1], uniqueN(penheader$ID)), metadata = penheader$metadata)
   penheader <- dcast(penheader, ID ~ header, value.var = 'metadata')
   penheader <- merge(penheader, dt.names, by = 'ID')
   # import data
   pen <- lapply(pen, fread, sep=';', skip = 14)
   pen <- rbindlist(pen, fill =F, use.names = F, idcol = "ID")
-  pen <- pen[, c("Depth", "Pen01" ,"Pen02" ,"Pen03" ,"Pen04", "Pen05", "Pen06", "Pen07", "Pen08", "Pen09" ,"Pen10") := tstrsplit(`Depth	Pen01	Pen02	Pen03	Pen04	Pen05	Pen06	Pen07	Pen08	Pen09	Pen10`, " ", fixed=TRUE)]
+  pen <- pen[, c("Diept", "Pen01" ,"Pen02" ,"Pen03" ,"Pen04", "Pen05", "Pen06", "Pen07", "Pen08", "Pen09" ,"Pen10") := tstrsplit(`Diept	Pen01	Pen02	Pen03	Pen04	Pen05	Pen06	Pen07	Pen08	Pen09	Pen10`, " ", fixed=TRUE)]
   
   # merge with metadata
   pen <- merge(pen, penheader, by = 'ID')
-  pen <- pen[,-c('Depth	Pen01	Pen02	Pen03	Pen04	Pen05	Pen06	Pen07	Pen08	Pen09	Pen10')]
+  pen <- pen[,-c('Diept	Pen01	Pen02	Pen03	Pen04	Pen05	Pen06	Pen07	Pen08	Pen09	Pen10')]
   
   # convert character 2 number
   changeCols <- colnames(pen[,2:12])
   pen <- pen[,(changeCols):= lapply(.SD, as.numeric), .SDcols = changeCols]
   
   # change format
-  pen <- melt.data.table(pen, id.vars = c('ID','Plotname','name','Depth','Cone type','Plotdate','Penetrationspeed'), 
+  pen <- melt.data.table(pen, id.vars = c('ID','Plotnaam','name','Diept','Conustype','Plotdatum','Penetratie snelheid'), 
                           measure.vars = c( "Pen01" ,"Pen02" ,"Pen03" ,"Pen04", "Pen05", "Pen06", "Pen07", "Pen08", "Pen09" ,"Pen10"),
                           variable.name = 'Pen', value.name = 'indringingsweerstand')
   
   return(pen)
 }
+
+# Visualise profielen-------------------------------------------------------------
+
+visualise_profiel<- function(proftest){
+  setDT(proftest)
+  zmin <- min(proftest$z-proftest$slib)
+  
+  maxdist_x <- ceiling(max(proftest$dist))
+  maxdist_y <- ceiling(max(proftest$z) - min(proftest$z))
+  maxdist <- max(maxdist_x,maxdist_y)
+  
+  ggplot() +
+    geom_line(data = proftest, aes(x = dist, y = z)) + 
+    geom_ribbon(data=proftest, aes(x= dist,ymin=zmin, ymax=z-slib), fill = 'lightgreen', alpha =0.5)+
+    geom_line(data = proftest, aes(x = dist, y = z-slib)) + 
+    geom_ribbon(data=proftest, aes(x= dist,ymin=z-slib, ymax=z), fill = 'brown', alpha =0.5)+
+    geom_line(data = proftest, aes(x = dist, y = wl)) + 
+    geom_ribbon(data=proftest, aes(x= dist,ymin=z, ymax=wl), fill = 'lightblue', alpha =0.5)+
+    coord_fixed(ratio =1)+
+    theme_minimal()+
+    theme(
+      strip.background = element_blank(),
+      strip.text.x = element_text(size=10), 
+      strip.text.y = element_text(size = 10), 
+      axis.text.x = element_text(size = 10),
+      axis.text.y = element_text(size= 10),
+      axis.ticks =  element_line(colour = "black"),
+      plot.title = element_text(size =12, face="bold", hjust = 0.5),
+      panel.background = element_blank(),
+      plot.background = element_blank(),
+    )+
+    ggtitle(paste0("Dwarsprofiel perceel en sloot op locatie ",proftest$name)) +
+    labs(x= "afstand in meters",y="diepte in mNAP")
+  ggsave(file=paste0('output/profielen/',proftest$name,"_",proftest$ID,'.png'),width = 40,height = 15,units='cm',dpi=1000)
+  
+}
+
+
--- scripts/main_veest.R
+++ scripts/main_veest.R
@@ -4,6 +4,7 @@ library(data.table)
 library(sf)
 library(R.utils)
 library(dplyr)
+library(ggplot2)
 
 # Settings ----------------------------------------------------------------
 workspace <- paste0(Sys.getenv("NMI-SITE"), 'O 1900 - O 2000/1922.N.23 VeeST vwsloot vd toekomst/05. Data/')
@@ -11,11 +12,12 @@ workspace <- paste0(Sys.getenv("NMI-SITE"), 'O 1900 - O 2000/1922.N.23 VeeST vws
 # Load custom functions-----------------------------------------------------
 source(paste0("scripts/functions/functions_veest.R"))
 
-# import gps dxf files------------------------------------------------------
+# import -------------------------------------------------------------------
+## import gps dxf files------------------------------------------------------
 gps <-  importGPS(inputdir = paste0(workspace,"./GPS"))
 gps <- st_collection_extract(gps, type = c("POINT"))
 
-# proces dxf files ---------------------------------------------------------
+## proces dxf files ---------------------------------------------------------
 # filter alleen de coordinaten van penetrometer eruit
 setDT(gps)
 gps <- gps[Layer == 'apglos_atts' & !is.na(Text) & !Text == "" & grepl('pen', Text),]
@@ -29,7 +31,6 @@ gps <- gps[!(name %in% c('ZG3b','ZG3a') & EntityHandle %in% unique(gps$EntityHan
 gps <- gps[!(name %in% c('ZG3b') & EntityHandle %in% unique(gps$EntityHandle[name == 'ZG3a'])),]
 gps <- gps[!(name %in% c('ZG2a') & EntityHandle %in% unique(gps$EntityHandle[name == 'ZG1b'])),]
 gps <- gps[!(name %in% c('ZG1b') & EntityHandle %in% unique(gps$EntityHandle[name == 'ZG1a'])),]
-
 # rondehoep sloot 1 tm 4, 7, 8, 10, zegveld 1 en 2, spaarnwoude is tweezijdig of twee transecten
 setDT(gps)
 gps[,trajecten := 2]
@@ -38,7 +39,7 @@ gps[,Opmerking := Text]
 # 1 = perceel, 2 = insteek, 3 tm 12 = oever en insteek en perceel andere oeverzijde
 gps <- gps[,dist_id := frank(EntityHandle, ties.method = 'dense'), by = 'ID']
 
-# import gps csv data
+## import gps csv data--------------------------------------------------------
 gps2 <-  importGPS2(inputdir = paste0(workspace,"./GPS"), gpsid = max(gps$ID))
 setDT(gps2)
 gps2[,trajecten := 1]
@@ -46,25 +47,33 @@ gps2[,trajecten := 1]
 # 1 = perceel, 2 = insteek, 3 tm 12 = oever en insteek en perceel andere oeverzijde
 gps2 <- gps2[,dist_id := frank(Puntnummer, ties.method = 'dense'), by = 'ID']
 
-# merge csv and dxf
+## merge csv and dxf--------------------------------------------------------
 gps <- gps[,c('ID','name','Opmerking','trajecten','dist_id','geometry')]
 gps2 <- gps2[,c('ID','name','Opmerking','trajecten','dist_id','geometry')]
 gps <- rbind(gps,gps2)
+#create id for filtering
+coords <- data.frame(st_coordinates(st_zm(gps$geometry)))
+gps <- cbind(gps,coords)
+gps[,ident:= paste0(X,'_',Y)]
 
-# add location info for filtering and correcting
+# Process gps files
+## add location info for filtering and correcting
 gps[, gebied:= tstrsplit(name, "_")[1]]
 gps[, sloot:= tstrsplit(name, "_")[2]]
 gps[ID %in% 29:36, sloot:= sub(".*?(\\d+).*", "\\1", name)]
 gps[ID %in% 29:36, gebied:= tstrsplit(name, sloot)[1]]
 # onderstaande sloten en opnamen wel eenzijdig (opvallend dat bij sommige sloten soms een, soms twee zijden penetrometer is gedaan)
 gps[gebied == 'Rh' & sloot %in% c('1','5','9','10')| name %in% c('Rh_7_M_Af_Kr', 'Rh_4_M_Af','Sw_1_M_Af', 'Sw_3_M')| gebied == "Kw" , trajecten := 1]
-
 # correct dist id for multiple transects (2 oevers or 2 transects)
 gps[,max_dis_id := max(dist_id), by = 'name']
 gps[trajecten == 2 & dist_id > (floor(max_dis_id/2)), dist_id := dist_id-floor(max_dis_id/2)]
 
+
 # let op coördinaten Krimpenerwaard missen: moeten uit word bestand worden gehaald en dist id moet handmatig worden toegevoegd
 # vragen aan Harm of hij id perceel, insteek en oever en oeverzijde controleert/toevoegt
+# nakijken een of tweezijdig coordinaten in relatie tot penetrometerdata
+# rondehoep sloot 1 is alleen gps data aan zuidzijde
+# let op consistentie naamgeving penetrometerdata gebied_plot_slootX zonder spaties
 
 # make unique name 4 matching
 gps[,Opmerking := tolower(Opmerking)]
@@ -76,15 +85,39 @@ gps[,plot := gsub("^p", "plot", plot)]
 gps[,plot := gsub("plotlot", "plot", plot)]
 gps[,gebied:= tolower(gebied)]
 gps[,oever:= tstrsplit(name, "_")[4]]
-gps[gebied == 'rh' & sloot %in% c(6,7,8,9,10), extragebied:= 'sloot6tm10']
-gps[gebied == 'rh' & sloot %in% c(1,2,3,4,5), extragebied:= 'sloot1tm5']
+gps[gebied == 'rh' & sloot %in% c(1,6,7,8,9,10), extragebied:= 'sloot6tm10'] #gps bevat alleen data van sloot 1 zuid, die hoort bij sloot6tm10
+gps[gebied == 'rh' & sloot %in% c(2,3,4,5), extragebied:= 'sloot1tm5']
 gps[gebied == 'rh' & sloot %in% c(11), extragebied:= 'sloot11']
+gps[gebied == 'sw' & sloot %in% c(4), extragebied:= 'sloot4']
+gps[gebied == 'sw' & sloot %in% c(5), extragebied:= 'sloot5']
+gps[gebied == 'sw' & sloot %in% c(6), extragebied:= 'sloot6']
+gps[grepl('WP1', name), extragebied:= 'wp1']
 
+# correctie dist_id
+gps[name == 'Rh_4_R' & Opmerking == 'plot6pen8', dist_id := 6]
+gps[name == 'Rh_4_R' & Opmerking == 'plot6pen9', dist_id := 1]
+gps[name == 'Rh_4_R' & Opmerking == 'plot6pen10', dist_id := 2]
+gps[name == 'Rh_4_R' & grepl('plot7', Opmerking), dist_id := dist_id-1]
+gps[name == 'Rh_11_M-AF_z' & grepl('plot1pen7', Opmerking), dist_id := 1]
+gps[name == 'Rh_11_M-AF_z' & grepl('plot1pen8', Opmerking), dist_id := 2]
+gps[name == 'Sw_3_M_Af' & grepl('plot5pen1', Opmerking), dist_id := 5]
+gps[name == 'Sw_3_M_Af' & grepl('plot5pen2', Opmerking), dist_id := 1]
+gps[name == 'Sw_3_M_Af' & grepl('plot5pen3', Opmerking), dist_id := 2]
+gps[name == 'Sw_3_M_Af' & grepl('plot5pen4', Opmerking), dist_id := 3]
+gps[name == 'Sw_3_M_Af' & grepl('plot5pen5', Opmerking), dist_id := 4]
+gps[name == 'Sw_2_M' & grepl('plot3pen9', Opmerking), dist_id := 4]
+gps[name == 'Sw_2_M' & grepl('plot3pen10', Opmerking), dist_id := 1]
+gps[name == 'Sw_2_M' & ident == '108210.754_489348.47', dist_id := 2]
+gps[name == 'Sw_2_M' & ident == '108210.754_489348.47', Opmerking := 'plot4pen1']
+gps[name == 'Sw_2_M' & grepl('plot4pen2', Opmerking), dist_id := 3]
+#ZG1b ZG1a
+#ZG2b ZG2a
 
-# Import penetrometer data ----------------------------------------------
+## Import penetrometer data ----------------------------------------------
 pen <- importPen(inputdir = paste0(workspace,"./Penetrometer"))
+pen <- pen[!is.na(indringingsweerstand),]
 # make unique name 4 matching
-pen[ ,plot:= as.numeric(gsub("^PLOTX*", "", Plotname))]
+pen[ ,plot:= as.numeric(gsub("^PLOTX*", "", Plotnaam))]
 pen[ ,pen := as.numeric(gsub("^Pen*", "", as.character(Pen)))]
 pen[ ,plot:= paste0('plot',plot,'pen',pen)]
 pen[ ,gebied:= tstrsplit(name, "_")[1]]
@@ -92,13 +125,13 @@ pen[ ,gebied:=tolower(gebied)]
 pen[ ,extragebied:= tstrsplit(name, "_")[3]]
 pen[ ,extragebied:= gsub('[<>.]','',extragebied)]
 
-# hoe weet ik wat rondehoep sloot 1noord en 1 zuid is in gps data?
+## Import abiotiek ---------------------------------------------------------
+## Import planing/behandelingen --------------------------------------------
+## Import slootprofielen ---------------------------------------------------
+profiel <-  importGPSprof(inputdir = paste0(workspace,"./GPS slootprofielen"))
 
-# Import abiotiek ---------------------------------------------------------
-# Import planing/behandelingen --------------------------------------------
-# Import slootprofielen ---------------------------------------------------
-# Import waterbodemdata ---------------------------------------------------
-# Import bodemgegevens oever ----------------------------------------------
+## Import waterbodemdata ---------------------------------------------------
+## Import bodemgegevens oever ----------------------------------------------
 
 # Validatie --------------------------------------------------
 
@@ -111,30 +144,88 @@ dubbel <- gps[which(duplicated(coords)),]
 # check of lijnen veldform abiotiek kruisen met penetrometer
 # check of lijnen veldform hetzelfde zijn als planningtabel
 # visualisatie behandelingen op kaart
-
 # validatieregels locatie(codes) en relatie met beheer/ behandelingen
 # check of beheerinvoer odk form == beheer planningstabel
 
 
-# Preprocessing/ aggregatie ---------------------------------------------
-# Preprocess penetrometer data 
+# Postprocessing/ aggregatie ---------------------------------------------
+
+## Postprocess penetrometer data ---------------------------------------------
 
 # merge gps with pen
 penmerge <- merge(pen, gps, by=c('gebied','plot','extragebied'), all.x = TRUE, all.y=FALSE, allow.cartesian = TRUE)
-#als ID.y = na : wel penetrometer, geen matching gps
+# check double gps
+checkgps <- gps[, nunique := uniqueN(ident), by = c('gebied','plot','extragebied')]
+# als ID.y = na : wel penetrometer, geen matching gps
 # sorteren op ID.x
 penmergecheck <- unique(penmerge[,c('name.y','ID.x','plot','gebied','sloot','extragebied','trajecten','dist_id')])
 write.table(penmergecheck, file = paste(workspace,"/penetrometer_gps_check",format(Sys.time(),"%Y%m%d%H%M"),".csv", sep= ""), quote = FALSE, na = "", sep =';', row.names = FALSE)
 gps <- st_as_sf(gps)
 st_write(gps, paste0(workspace,  "gps_penetrometer.gpkg"))
-
 # soms meerdere gps coordinaten bij 1 gebied, plot, extragebied 
-# merge met abiotiek kan op oever holling na
+# merge met abiotiek aan een verkeerde zijde kan op oever holling na
 # merge met vegetatie hoe? dan moet ik ook oeverzijde hebben
+
+
+## Postprocess profielen-----------------------------------------------------
+# Mijnden
+# 1: 3-34, 2: 35-50, 3:69-82, 4: 51-68, 5: 83-98, 6: 99-122, 123:154, 155:183, 184:215, 216:246,280:314, 247:279,  315:350, 351:386,387:429, 430:471
+setDT(profiel)
+profiel[,ID := as.numeric(ID)]
+profiel[name == 'polder mijnden zuid' & Puntnummer >= 3 & Puntnummer <= 34, ID := paste0(ID,'.',1) ]
+profiel[name == 'polder mijnden zuid' & Puntnummer >= 35 & Puntnummer <= 50, ID := paste0(ID,'.',2) ]
+profiel[name == 'polder mijnden zuid' & Puntnummer >= 51 & Puntnummer <= 68, ID := paste0(ID,'.',3) ]
+profiel[name == 'polder mijnden zuid' & Puntnummer >= 69 & Puntnummer <= 82, ID := paste0(ID,'.',4) ]
+profiel[name == 'polder mijnden zuid' & Puntnummer >= 83 & Puntnummer <= 98, ID := paste0(ID,'.',5) ]
+profiel[name == 'polder mijnden zuid' & Puntnummer >= 99 & Puntnummer <= 122, ID := paste0(ID,'.',6) ]
+profiel[name == 'polder mijnden zuid' & Puntnummer >= 123 & Puntnummer <= 154, ID := paste0(ID,'.',7) ]
+profiel[name == 'polder mijnden zuid' & Puntnummer >= 155 & Puntnummer <= 183, ID := paste0(ID,'.',8) ]
+profiel[name == 'polder mijnden zuid' & Puntnummer >= 184 & Puntnummer <= 215, ID := paste0(ID,'.',9) ]
+profiel[name == 'polder mijnden zuid' & Puntnummer >= 216 & Puntnummer <= 246, ID := paste0(ID,'.',11) ]
+profiel[name == 'polder mijnden zuid' & Puntnummer >= 247 & Puntnummer <= 279, ID := paste0(ID,'.',12) ]
+profiel[name == 'polder mijnden zuid' & Puntnummer >= 280 & Puntnummer <= 314, ID := paste0(ID,'.',13) ]
+profiel[name == 'polder mijnden zuid' & Puntnummer >= 315 & Puntnummer <= 350, ID := paste0(ID,'.',14) ]
+profiel[name == 'polder mijnden zuid' & Puntnummer >= 351 & Puntnummer <= 386, ID := paste0(ID,'.',15) ]
+profiel[name == 'polder mijnden zuid' & Puntnummer >= 387 & Puntnummer <= 429, ID := paste0(ID,'.',16) ]
+profiel[name == 'polder mijnden zuid' & Puntnummer >= 430 & Puntnummer <= 471, ID := paste0(ID,'.',17) ]
+
+profiel[name == 'polder mijnden zuid' & Puntnummer == 3 , Puntnummer := 1 ]
+profiel[name == 'polder mijnden zuid' & Puntnummer == 35 , Puntnummer := 1 ]
+profiel[name == 'polder mijnden zuid' & Puntnummer == 51 , Puntnummer := 1 ]
+profiel[name == 'polder mijnden zuid' & Puntnummer == 69 , Puntnummer := 1 ]
+profiel[name == 'polder mijnden zuid' & Puntnummer == 83 , Puntnummer := 1 ]
+profiel[name == 'polder mijnden zuid' & Puntnummer == 99 , Puntnummer := 1 ]
+profiel[name == 'polder mijnden zuid' & Puntnummer == 123 , Puntnummer:= 1 ]
+profiel[name == 'polder mijnden zuid' & Puntnummer == 155 , Puntnummer := 1 ]
+profiel[name == 'polder mijnden zuid' & Puntnummer == 184 , Puntnummer := 1 ]
+profiel[name == 'polder mijnden zuid' & Puntnummer == 216 , Puntnummer:= 1 ]
+profiel[name == 'polder mijnden zuid' & Puntnummer == 247 , Puntnummer:= 1]
+profiel[name == 'polder mijnden zuid' & Puntnummer == 280 , Puntnummer := 1]
+profiel[name == 'polder mijnden zuid' & Puntnummer == 315 , Puntnummer:= 1 ]
+profiel[name == 'polder mijnden zuid' & Puntnummer == 351 , Puntnummer:= 1 ]
+profiel[name == 'polder mijnden zuid' & Puntnummer == 387 , Puntnummer:= 1 ]
+profiel[name == 'polder mijnden zuid' & Puntnummer == 430 , Puntnummer:= 1]
+
+# nog checken dit is afstand in meters toch?
+profiel[, dist := sqrt((x[Puntnummer == 1]-x)^2+(y[Puntnummer == 1]-y)^2), by ='ID'] 
 
 # Visualisaties ---------------------------------------------------------
 
+# plot profiel
+for(i in unique(profiel$ID)){
+  setDT(profiel)
+  visualise_profiel(profiel[ID == i,])
+  print(i)
+}
+
+# test voor Mijnden
+# install.packages("scatterplot3d") # Install
+library("scatterplot3d") # load
+proftest <- profiel[ID == 1,]
+scatterplot3d(proftest$x,proftest$y, proftest$z)
+plot(proftest$dist,proftest$z)  
   
+
 # Export the data ---------------------------------------------------------
 saveRDS(report, paste0(workspace, 'products/', snapshot.version, '/report.rds'))
 
